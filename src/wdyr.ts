80f20fa1e6da3846588a9ea04fd186bdf6c57f5c
Ahsan Mahmood <aoneahsan@gmail.com>
2023-11-27 14:16:19 +0500

    v2-f-dev - why-did-you-render package added and working fine right now

---
+++ src/wdyr.ts
@@ -0,0 +1,12 @@
+/// <reference types="@welldone-software/why-did-you-render" />
+
+import React from 'react';
+
+if (process.env.NODE_ENV === 'development') {
+  const { default: whyDidYouRender } = await import(
+    '@welldone-software/why-did-you-render'
+  );
+  whyDidYouRender(React, {
+    trackAllPureComponents: true
+  });
+}
