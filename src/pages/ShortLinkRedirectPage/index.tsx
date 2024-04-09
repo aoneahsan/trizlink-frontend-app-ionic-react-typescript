1f756df869b6a03abc0d590bcc2cb4c29e8846f9
Ahsan Mahmood <aoneahsan@gmail.com>
2024-04-09 18:10:57 +0500

    v2-f-dev - build errors resolved

--- src/pages/ShortLinkRedirectPage/index.tsx
+++ src/pages/ShortLinkRedirectPage/index.tsx
@@ -46,7 +46,7 @@ import { useZRQCreateRequest } from '@/ZaionsHooks/zreactquery-hooks';
  * Global Constants Imports go down
  * ? Like import of Constant is a global constants import
  * */
-import CONSTANTS, { ExternalURL } from '@/utils/constants';
+import CONSTANTS, { ProductExternalURL } from '@/utils/constants';
 import { API_URL_ENUM, extractInnerDataOptionsEnum } from '@/utils/enums';
 import { ENVS } from '@/utils/envKeys';
 import {
