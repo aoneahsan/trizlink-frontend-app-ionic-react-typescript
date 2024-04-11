0d4046fef4a52c051a22ac7b92d70bb00e4dcb09
Ahsan Mahmood <aoneahsan@gmail.com>
2024-04-11 15:51:29 +0500

    v2-f-dev - sentry source maps config and packages added in project using the sentry-wizard global package '@sentry/wizard', tried to build the project got errors, now resolving these errors to move forward

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import { sentryVitePlugin } from "@sentry/vite-plugin";
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -8,15 +9,24 @@ const __dirname = path.dirname(__filename);
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), sentryVitePlugin({
+    org: "trizlink",
+    project: "trizlink-frontend-local"
+  })],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   },
+
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts'
+  },
+
+  build: {
+    sourcemap: true
   }
 });
